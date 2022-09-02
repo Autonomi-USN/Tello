555eafd2704c1fa1afc1132a97fa394208bb48fb
sinxno <smith.jahnsen@gmail.com>
2022-09-02 13:24:36 +0200

    Publishes video stream to "image_tello" topic

--- tello_bridge/src/tello_bridge_wrapper.py
+++ tello_bridge/src/tello_bridge_wrapper.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python
-from ast import If
-import rospy 
-from geometry_msgs.msg import Twist
+import rospy
+import cv2
+import numpy as np
 from djitellopy import Tello
+from geometry_msgs.msg import Twist
 from tello_bridge.msg import Tello_data
+from cv_bridge import CvBridge, CvBridgeError
+from sensor_msgs.msg import Image
+
 
 class Tello_Bridge_Node:
     def __init__(self):
         rospy.init_node("tello_bridge_node")
         rospy.loginfo("Starting Tello_Bridge_Node.")
+
+        self.cvBridge = CvBridge()
+        self.tello = Tello()
+
         #a: left/right -100 = 100% left, +100 = 100% right
         #b: forward/backward -100 = 100% back, +100 = 100% forward
         #c: up/down -100 = 100% down, +100 = 100% up
@@ -16,17 +24,23 @@ class Tello_Bridge_Node:
         self.rc = {'a': 0, 'b': 0, 'c' : 0, 'd' : 0}
         self.speed = 50
         self.data_msg = Tello_data()
-        self.tello = Tello()
+        
         rospy.Subscriber("/cmd_vel", Twist, self.callback)
         self.pub = rospy.Publisher("tello_data", Tello_data, queue_size=10)
-        
+        self.image_pub = rospy.Publisher("image_topic_tello", Image)
+        self.command_frequency = 1.0/10.0
 
         self.tello.connect()
         self.tello.set_speed(self.speed)
         self.tello.streamoff()
+        self.tello.set_video_resolution(self.tello.RESOLUTION_480P)
         self.tello_get_data()
-        self.tello.takeoff()
-        rospy.Timer(rospy.Duration(1.0/10.0),self.send_to_tello)
+        self.tello.streamon()
+        self.frame_read = self.tello.get_frame_read()
+        #self.tello.takeoff()
+        self.frame = self.frame_read.frame
+        #Calls communication 
+        rospy.Timer(rospy.Duration(self.command_frequency),self.send_to_tello)
 
         rospy.spin()
 
@@ -37,8 +51,15 @@ class Tello_Bridge_Node:
         self.rc['d'] = int(clamp(Twist.angular.z * -self.speed, -100, 100))
 
     def send_to_tello(self, event=None):
-        self.tello.send_rc_control(self.rc['a'], self.rc['b'], self.rc['c'], self.rc['d'],)
+        """
+        Sends remote controll data to the drone
+
+        Args:
+            event (_type_, optional): _description_. Defaults to None.
+        """
+        #self.tello.send_rc_control(self.rc['a'], self.rc['b'], self.rc['c'], self.rc['d'],)
         self.tello_get_data()
+        self.tello_get_video()
 
     def tello_shutdown_sequence(self):
         print('Attempting to land drone')
@@ -46,6 +67,9 @@ class Tello_Bridge_Node:
 
 
     def tello_get_data(self):
+        """
+        Gets data from the drone and publishes it on the tello_data topic
+        """
         self.data_msg.pitch = int(self.tello.get_pitch())
         self.data_msg.roll = int(self.tello.get_roll())
         self.data_msg.yaw = int(self.tello.get_yaw())
@@ -60,9 +84,38 @@ class Tello_Bridge_Node:
         self.data_msg.battery = int(self.tello.get_battery())
         self.pub.publish(self.data_msg)
 
+    def tello_get_video(self):
+        self.frame = self.frame_read.frame
+        #self.frame = cv2.resize(self.frame, (320,240))
+        self.frame = self.frame[0:480, 0:640]
+        #self.frame = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
+        #cv2.imshow("result", self.frame)
+        #cv2.waitKey(1)
+        #self.frame = cv2.cvtColor(self.frame, cv2.COLORBGR2RGB)
+        #self.frame = np.rot90(self.frame)
+        #self.frame = np.flipud(self.frame)
+        self.image_message = self.cvBridge.cv2_to_imgmsg(self.frame, encoding="passthrough")
+        try:
+            self.image_pub.publish(self.image_message)
+        except CvBridgeError as e:
+            print(e)
+
+
+
 
         
 def clamp(n, minn, maxn):
+    """
+    Clamps a value
+
+    Args:
+        n (int/float): The number to clamp
+        minn (int/float): Minimum value
+        maxn (int/float): Maximum value
+
+    Returns:
+        int/float: Clamped value
+    """
     return max(min(maxn, n), minn)
 
 if __name__ == "__main__":
