a55e9f0ca7070762986186490892b28c7863efe5
sinxno <smith.jahnsen@gmail.com>
2022-11-16 14:42:12 +0100

    Telemetry data implemented with tellopy

--- tello_bridge/src/tello_bridge_wrapper.py
+++ tello_bridge/src/tello_bridge_wrapper.py
@@ -6,7 +6,7 @@ from geometry_msgs.msg import Twist
 from tello_bridge.msg import Tello_data
 from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import Image
-import tellopy
+from tellopy import tello
 import time
 import sys
 from subprocess import Popen, PIPE
@@ -14,7 +14,6 @@ import threading
 import av
 import traceback
 
-#TODO: change tello implementation to low level: https://github.com/hanyazou/TelloPy/tree/develop-0.7.0/tellopy/examples
 
 class Tello_Bridge_Node:
     def __init__(self):
@@ -24,6 +23,7 @@ class Tello_Bridge_Node:
         self.prev_flight_data = None
         self.run_recv_thread = True
         self.new_image = None
+        self.current_image = None
         self.flight_data = None
         self.log_data = None
         self.speed = 100
@@ -33,7 +33,7 @@ class Tello_Bridge_Node:
         self.roll = 0.0
 
         self.cvBridge = CvBridge()
-        self.telloNew = tellopy.Tello()
+        self.telloNew = tello.Tello()
 
         #a: left/right -100 = 100% left, +100 = 100% right
         #b: forward/backward -100 = 100% back, +100 = 100% forward
@@ -46,29 +46,28 @@ class Tello_Bridge_Node:
         self.tello_data_pub = rospy.Publisher("tello_data", Tello_data, queue_size=10)
         self.image_pub = rospy.Publisher("image_tello", Image, queue_size=10)
         self.command_frequency = 1.0/10.0
-        self.telloNew.subscribe(self.telloNew.EVENT_FLIGHT_DATA, self.flightDataHandler)
-        self.telloNew.subscribe(self.telloNew.EVENT_VIDEO_FRAME, self.videoFrameHandler)
-        self.telloNew.subscribe(self.telloNew.EVENT_FILE_RECEIVED, self.handleFileRecieved)
-
         self.telloNew.connect()
         self.telloNew.wait_for_connection(60)
+        self.telloNew.subscribe(self.telloNew.EVENT_FLIGHT_DATA, self.handler)
+        self.telloNew.subscribe(self.telloNew.EVENT_LOG_DATA, self.handler)
+        
+        """
+        
         self.telloNew.set_roll(self.speed/100.0)
         self.telloNew.set_pitch(self.speed/100.0)
         self.telloNew.set_yaw(self.speed/100.0)
         self.telloNew.set_throttle(self.speed/100.0)
+        """
+        self.telloNew.set_roll(0)
+        self.telloNew.set_pitch(0)
+        self.telloNew.set_yaw(0)
+        self.telloNew.set_throttle(0)
+
         
-        self.tello.streamoff()
-        self.tello.set_video_resolution(self.tello.RESOLUTION_480P)
-        self.height_base = self.tello.get_barometer()
-        self.tello_get_data()
-        self.tello.streamon()
-        self.frame_read = self.tello.get_frame_read()
-        self.frame = self.frame_read.frame
-        self.tello_get_video()
-        self.tello.takeoff()
         
-        #Calls communication 
-        rospy.Timer(rospy.Duration(self.command_frequency),self.send_to_tello)
+        threading.Thread(target=self.recv_thread, args=[self.telloNew]).start()
+        
+        #self.telloNew.takeoff()
 
         rospy.spin()
 
@@ -78,17 +77,24 @@ class Tello_Bridge_Node:
         self.rc['c'] = int(clamp(Twist.linear.z * self.speed, -100, 100))
         self.rc['d'] = int(clamp(Twist.angular.z * -self.speed, -100, 100))
 
-    def recv_thread(self, drone):
-        self.run_recv_thread
-        self.new_image
-        self.flight_data
-        self.log_data
+    def handler(self, event, sender, data, **args):
+        drone = sender
+        if event is drone.EVENT_FLIGHT_DATA:
+            if self.prev_flight_data != str(data):
+                print(data)
+                self.prev_flight_data = str(data)
+            self.flight_data = data
+        elif event is drone.EVENT_LOG_DATA:
+            self.log_data = data
+        else:
+            print('event="%s" data=%s' % (event.getname(), str(data)))
 
+    def recv_thread(self, drone):
         print('start recv_thread()')
         try:
             container = av.open(drone.get_video_stream())
-            # skip first 300 frames
-            frame_skip = 300
+            # skip first 30 frames
+            frame_skip = 30
             while True:
                 for frame in container.decode(video=0):
                     if 0 < frame_skip:
@@ -96,14 +102,18 @@ class Tello_Bridge_Node:
                         continue
                     start_time = time.time()
                     image = cv2.cvtColor(np.array(frame.to_image()), cv2.COLOR_RGB2BGR)
+                    self.frame = image
+                    self.image_message = self.cvBridge.cv2_to_imgmsg(self.frame, encoding="passthrough")
+                    try:
+                        self.image_pub.publish(self.image_message)
+                    except CvBridgeError as e:
+                        print(e)
 
                     if self.flight_data:
-                        print('TelloPy: joystick_and_video ' + str(self.flight_data))
+                        self.tello_flight_data_publish()
                         
                     if self.log_data:
-                        print('MVO: ' + str(self.log_data.mvo))
-                        print('IMU: ' + str(self.log_data.imu)[0:52])
-                        print('     ' + ('IMU: ' + str(self.log_data.imu)[52:]))
+                        self.tello_log_data_publish()
                         
                     self.new_image = image
                     if frame.time_base < 1.0/60:
@@ -124,43 +134,44 @@ class Tello_Bridge_Node:
             event (_type_, optional): _description_. Defaults to None.
         """
         self.tello.send_rc_control(self.rc['a'], self.rc['b'], self.rc['c'], self.rc['d'],)
-        self.tello_get_data()
-        self.tello_get_video()
 
     def tello_shutdown_sequence(self):
         print('Attempting to land drone')
-        self.tello.end()
+        self.telloNew.land()
+        self.run_recv_thread = False
+        self.telloNew.quit()
+        
 
 
-    def tello_get_data(self):
-        """
-        Gets data from the drone and publishes it on the tello_data topic
-        """
-        self.data_msg.pitch = int(self.tello.get_pitch())
-        self.data_msg.roll = int(self.tello.get_roll())
-        self.data_msg.yaw = int(self.tello.get_yaw())
-        self.data_msg.speed_x = self.tello.get_speed_x()/10.0
-        self.data_msg.speed_y = self.tello.get_speed_y()/10.0
-        self.data_msg.speed_z = self.tello.get_speed_z()/10.0
-        self.data_msg.acceleration_x = self.tello.get_acceleration_x()/100.0
-        self.data_msg.acceleration_y = self.tello.get_acceleration_y()/100.0
-        self.data_msg.acceleration_z = self.tello.get_acceleration_z()/100.0
-        self.data_msg.rel_height = ((self.height_base - self.tello.get_barometer())/100.0)*-1.0
-        self.data_msg.abs_height = self.tello.get_barometer()/100.0
-        self.data_msg.tof_height = self.tello.get_distance_tof()/100.0
-        self.data_msg.battery = int(self.tello.get_battery())
+    def tello_log_data_publish(self):
+        self.data_msg.vel_x = self.log_data.mvo.vel_x
+        self.data_msg.vel_y = self.log_data.mvo.vel_y
+        self.data_msg.vel_z = self.log_data.mvo.vel_z
+        self.data_msg.pos_x = self.log_data.mvo.pos_x
+        self.data_msg.pos_y = self.log_data.mvo.pos_y
+        self.data_msg.pos_z = self.log_data.mvo.pos_z
+        self.data_msg.acc_x = self.log_data.imu.acc_x
+        self.data_msg.acc_y = self.log_data.imu.acc_y
+        self.data_msg.acc_z = self.log_data.imu.acc_z
+        self.data_msg.gyro_x = self.log_data.imu.gyro_x
+        self.data_msg.gyro_y = self.log_data.imu.gyro_y
+        self.data_msg.gyro_z = self.log_data.imu.gyro_z
+        self.data_msg.vg_x = self.log_data.imu.vg_x
+        self.data_msg.vg_y = self.log_data.imu.vg_y
+        self.data_msg.vg_z = self.log_data.imu.vg_z
+        self.data_msg.q0 = self.log_data.imu.q0
+        self.data_msg.q1 = self.log_data.imu.q1
+        self.data_msg.q2 = self.log_data.imu.q2
+        self.data_msg.q3 = self.log_data.imu.q3
         self.tello_data_pub.publish(self.data_msg)
 
-    def tello_get_video(self):
-        """Publishes video stream to image_tello
-        """
-        self.frame = self.frame_read.frame
-        self.frame = self.frame[0:480, 0:640]
-        self.image_message = self.cvBridge.cv2_to_imgmsg(self.frame, encoding="passthrough")
-        try:
-            self.image_pub.publish(self.image_message)
-        except CvBridgeError as e:
-            print(e)
+    def tello_flight_data_publish(self):
+        self.data_msg.battery_percentage = self.flight_data.battery_percentage
+        self.data_msg.east_speed = self.flight_data.east_speed
+        self.data_msg.north_speed = self.flight_data.north_speed
+        self.data_msg.ground_speed = self.flight_data.ground_speed
+        self.data_msg.height = self.flight_data.height
+        self.tello_data_pub.publish(self.data_msg)
 
 
 
