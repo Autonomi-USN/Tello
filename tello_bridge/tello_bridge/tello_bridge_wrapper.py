93a655823def3c16d08eabf188ccef240ca33d9f
Marcos Moura <gabbyru2@gmail.com>
2024-10-03 17:11:53 +0200

    feat: add tello_imu and tello_odom publishers

--- tello_bridge/tello_bridge/tello_bridge_wrapper.py
+++ tello_bridge/tello_bridge/tello_bridge_wrapper.py
@@ -11,12 +11,12 @@ import traceback
 import time
 import sys
 from tello_bridge.tellopy.event import Event
-from subprocess import Popen, PIPE
 import tello_bridge.tellopy.tello as tello
 import tello_bridge.tellopy.error as error
 from rclpy.action import ActionServer
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import Image, Imu
 from geometry_msgs.msg import Twist
+from nav_msgs.msg import Odometry
 from tello_interfaces.msg import TelloData
 from tello_interfaces.action import Takeoff, Land
 
@@ -25,6 +25,17 @@ class TelloBridgeNode(Node):
         super().__init__('tello_bridge_node')
         self.get_logger().info("Starting TelloBridgeNode.")
 
+        self.initialize_variables()
+        
+        self.setup_action_servers()
+        self.setup_subscribers()
+        self.setup_publishers()
+        
+        self.tello_connect()
+
+    # ------ Initializing the system ------ #
+    
+    def initialize_variables(self) -> None:
         self.prev_flight_data = None
         self.run_recv_thread = True
         self.new_image = None
@@ -44,18 +55,21 @@ class TelloBridgeNode(Node):
         self.takeoff_feedback = Takeoff.Feedback()
         self.takeoff_result = Takeoff.Result()
 
+
+    def setup_publishers(self) -> None:
+        self.tello_data_pub = self.create_publisher(TelloData, '/tello_data', 10)
+        self.tello_imu_pub = self.create_publisher(Imu, '/tello_imu', 10)
+        self.tello_odom_pub = self.create_publisher(Odometry, '/tello_odom', 10)
+        self.image_pub = self.create_publisher(Image, '/tello_image', 10)
+
+    def setup_subscribers(self) -> None:
         self.create_subscription(Twist, '/cmd_vel', self.cmd_vel_cb, 10)
+
+    def setup_action_servers(self) -> None:
         self.as_takeoff = ActionServer(self, Takeoff, '/tello_takeoff', self.takeoff_execute_cb)
         self.as_land = ActionServer(self, Land, '/tello_land', self.land_execute_cb)
         self.get_logger().info("Takeoff and land action servers started")
 
-        self.tello_data_pub = self.create_publisher(TelloData, 'tello_data', 10)
-        self.image_pub = self.create_publisher(Image, 'tello_image', 10)
-        
-        self.tello_connect()
-        self.tello.subscribe(self.tello.EVENT_FLIGHT_DATA, self.event_handler)
-        self.tello.subscribe(self.tello.EVENT_LOG_DATA, self.event_handler)
-        threading.Thread(target=self.recv_thread, args=[self.tello]).start()
 
     def tello_connect(self) -> None:
         self.connection_quit = True
@@ -71,50 +85,45 @@ class TelloBridgeNode(Node):
         self.get_logger().info("Connected to Tello drone")
         self.connection_quit = False
         
-
+        self.tello.subscribe(self.tello.EVENT_FLIGHT_DATA, self.event_handler)
+        self.tello.subscribe(self.tello.EVENT_LOG_DATA, self.event_handler)
+        threading.Thread(target=self.recv_thread, args=[self.tello]).start()
+        
+    # ------ Action Server Handling ------ #
+    
     def takeoff_execute_cb(self, goal_handle: Takeoff) -> None:
         self.get_logger().info("Attempting to takeoff drone")
-        self.tello.takeoff()
-        count = 0
-        timer = time.time()
-        self.takeoff_result.success = True
-        while self.flight_data.em_sky == 0:
-            if time.time() - timer > 1:
-                timer = time.time()
-                count += 1
-                self.takeoff_feedback.seconds_taken = count
-                goal_handle.publish_feedback(self.takeoff_feedback)
-            if count > 20:
-                self.takeoff_result.success = False
-                self.get_logger().error("Failed to takeoff drone")
-                break
-        if self.takeoff_result.success:
-            self.landed = False
-            self.get_logger().info("Takeoff successful")
-        goal_handle.succeed()
-        goal_handle.set_result(self.takeoff_result)
+        self.execute_drone_action(self.tello.takeoff, goal_handle, "Takeoff", 20)
 
     def land_execute_cb(self, goal_handle: Land) -> None:
         self.get_logger().info("Attempting to land drone")
-        self.tello.land()
+        self.execute_drone_action(self.tello.land, goal_handle, "Landing", 10)
+
+    def execute_drone_action(self, action_func, goal_handle, action_name, timeout) -> None:
+        action_func()
+        feedback, result = (self.takeoff_feedback, self.takeoff_result) if action_name == "Takeoff" else (self.land_feedback, self.land_result)
+        result.success = True
+
         count = 0
         timer = time.time()
-        self.land_result.success = True
-        while self.flight_data.em_sky != 0:
+        while self.flight_data.em_sky == (0 if action_name == "Takeoff" else 1):
             if time.time() - timer > 1:
                 timer = time.time()
                 count += 1
-                self.land_feedback.seconds_taken = count
-                goal_handle.publish_feedback(self.land_feedback)
-            if count > 10:
-                self.land_result.success = False
-                self.get_logger().error("Failed to land drone")
+                feedback.seconds_taken = count
+                goal_handle.publish_feedback(feedback)
+            if count > timeout:
+                result.success = False
+                self.get_logger().error(f"Failed to {action_name.lower()} drone")
                 break
-        if self.land_result.success:
-            self.landed = True
-            self.get_logger().info("Landing successful")
+
+        if result.success:
+            self.landed = (action_name == "Landing")
+            self.get_logger().info(f"{action_name} successful")
         goal_handle.succeed()
-        goal_handle.set_result(self.land_result)
+        goal_handle.set_result(result)
+
+    # ------ Subscriber Callback Handling ------ #
 
     def cmd_vel_cb(self, msg: Twist) -> None:
         if self.flight_data.em_sky != 0:
@@ -122,6 +131,8 @@ class TelloBridgeNode(Node):
             self.tello.set_roll(self.clamp(-msg.linear.y, -1.0, 1.0))
             self.tello.set_yaw(self.clamp(-msg.angular.z, -1.0, 1.0))
             self.tello.set_throttle(self.clamp(msg.linear.z, -1.0, 1.0))
+
+    # ------ Tello Data Handling ------ #
 
     def event_handler(self, event: Event, sender: tello.Tello, data: Any, **args) -> None:
         drone = sender
@@ -135,49 +146,54 @@ class TelloBridgeNode(Node):
     def recv_thread(self, drone: tello.Tello) -> None:
         self.get_logger().info("Tello receive data thread started")
         if self.handle_img:
-            try:
-                container = av.open(drone.get_video_stream())
-                frame_skip = 30
-                while True:
-                    for frame in container.decode(video=0):
-                        if frame_skip > 0:
-                            frame_skip -= 1
-                            continue
-                        start_time = time.time()
-                        image = cv2.cvtColor(np.array(frame.to_image()), cv2.COLOR_RGB2BGR)
-                        self.frame = image
-                        self.image_message = self.cvBridge.cv2_to_imgmsg(self.frame, encoding="passthrough")
-                        try:
-                            self.image_pub.publish(self.image_message)
-                        except CvBridgeError as e:
-                            self.get_logger().error(str(e))
-
-                        if self.flight_data:
-                            self.tello_flight_data_publish()
-
-                        if self.log_data:
-                            self.tello_log_data_publish()
-
-                        self.new_image = image
-                        time_base = frame.time_base if frame.time_base >= 1.0/60 else 1.0/60
-                        frame_skip = int((time.time() - start_time) / time_base)
-            except Exception as ex:
-                exc_type, exc_value, exc_traceback = sys.exc_info()
-                traceback.print_exception(exc_type, exc_value, exc_traceback)
-                print(ex)
+            self.handle_video_stream(drone)
         else:
-            try:
-                while True:
-                    start_time = time.time()
-                    if self.flight_data:
-                        self.tello_flight_data_publish()
+            self.process_flight_data_loop()
 
-                    if self.log_data:
-                        self.tello_log_data_publish()
-            except Exception as ex:
-                exc_type, exc_value, exc_traceback = sys.exc_info()
-                traceback.print_exception(exc_type, exc_value, exc_traceback)
-                print(ex)
+    def handle_video_stream(self, drone: tello.Tello) -> None:
+        try:
+            container = av.open(drone.get_video_stream())
+            frame_skip = 30
+            while True:
+                for frame in container.decode(video=0):
+                    if frame_skip > 0:
+                        frame_skip -= 1
+                        continue
+                    start_time = time.time()
+                    image = cv2.cvtColor(np.array(frame.to_image()), cv2.COLOR_RGB2BGR)
+                    self.publish_image(image)
+                    self.new_image = image
+                    frame_skip = int((time.time() - start_time) / max(frame.time_base, 1.0/60))
+        except Exception as ex:
+            self.handle_exception(ex)
+
+    def process_flight_data_loop(self) -> None:
+        try:
+            while True:
+                self.process_flight_data()
+        except Exception as ex:
+            self.handle_exception(ex)
+
+    def process_flight_data(self) -> None:
+        if self.flight_data:
+            self.tello_flight_data_publish()
+
+        if self.log_data:
+            self.tello_log_data_publish()
+            self.tello_imu_publish()
+            self.tello_odom_publish()
+
+    def shutdown(self) -> None:
+        self.run_recv_thread = False
+        self.get_logger().info("Shutting down TelloBridgeNode")
+        self.tello.quit()
+        rclpy.shutdown()
+
+    def handle_exception(self, exception: Exception) -> None:
+        self.get_logger().error(f"Exception occurred: {str(exception)}")
+        self.get_logger().error(traceback.format_exc())
+
+    # ------ Publisher Functions ------ #
 
     def tello_shutdown_sequence(self) -> None:
         if not self.connection_quit:
@@ -195,6 +211,7 @@ class TelloBridgeNode(Node):
         self.data_msg.pos_x = self.log_data.mvo.pos_x
         self.data_msg.pos_y = self.log_data.mvo.pos_y
         self.data_msg.pos_z = self.log_data.mvo.pos_z
+
         self.data_msg.acc_x = self.log_data.imu.acc_x
         self.data_msg.acc_y = self.log_data.imu.acc_y
         self.data_msg.acc_z = self.log_data.imu.acc_z
@@ -208,8 +225,51 @@ class TelloBridgeNode(Node):
         self.data_msg.q1 = self.log_data.imu.q1
         self.data_msg.q2 = self.log_data.imu.q2
         self.data_msg.q3 = self.log_data.imu.q3
+
         self.data_msg.landed = self.landed
         self.tello_data_pub.publish(self.data_msg)
+
+    def tello_imu_publish(self) -> None:
+        imu_msg = Imu()
+        imu_msg.header.stamp = self.get_clock().now().to_msg()
+
+        imu_msg.orientation.x = self.log_data.imu.q0
+        imu_msg.orientation.y = self.log_data.imu.q1
+        imu_msg.orientation.z = self.log_data.imu.q2
+        imu_msg.orientation.w = self.log_data.imu.q3
+
+        imu_msg.angular_velocity.x = self.log_data.imu.gyro_x
+        imu_msg.angular_velocity.y = self.log_data.imu.gyro_y
+        imu_msg.angular_velocity.z = self.log_data.imu.gyro_z
+
+        imu_msg.linear_acceleration.x = self.log_data.imu.acc_x
+        imu_msg.linear_acceleration.y = self.log_data.imu.acc_x
+        imu_msg.linear_acceleration.z = self.log_data.imu.acc_x
+
+        self.tello_imu_pub.publish(imu_msg)
+
+    def tello_odom_publish(self) -> None:
+        odom_msg = Odometry()
+        odom_msg.header.stamp = self.get_clock().now().to_msg()
+        
+        odom_msg.pose.pose.position.x = self.log_data.mvo.pos_x
+        odom_msg.pose.pose.position.y = self.log_data.mvo.pos_y
+        odom_msg.pose.pose.position.z = self.log_data.mvo.pos_z
+
+        odom_msg.pose.pose.orientation.x = self.log_data.imu.q0
+        odom_msg.pose.pose.orientation.y = self.log_data.imu.q1
+        odom_msg.pose.pose.orientation.z = self.log_data.imu.q2
+        odom_msg.pose.pose.orientation.w = self.log_data.imu.q3
+
+        odom_msg.twist.twist.linear.x = self.log_data.mvo.pos_x
+        odom_msg.twist.twist.linear.y = self.log_data.mvo.pos_y
+        odom_msg.twist.twist.linear.z = self.log_data.mvo.pos_z
+
+        odom_msg.twist.twist.angular.x = self.log_data.imu.gyro_x
+        odom_msg.twist.twist.angular.y = self.log_data.imu.gyro_y
+        odom_msg.twist.twist.angular.z = self.log_data.imu.gyro_z
+        
+        self.tello_odom_pub.publish(odom_msg)
 
     def tello_flight_data_publish(self) -> None:
         self.data_msg.battery_percentage = self.flight_data.battery_percentage
@@ -219,6 +279,13 @@ class TelloBridgeNode(Node):
         self.data_msg.height = self.flight_data.height / 10.0
         self.data_msg.landed = self.landed
         self.tello_data_pub.publish(self.data_msg)
+
+    def publish_image(self, image: np.ndarray) -> None:
+        try:
+            img_msg = self.cvBridge.cv2_to_imgmsg(image, encoding="bgr8")
+            self.image_pub.publish(img_msg)
+        except CvBridgeError as err:
+            self.get_logger().error(str(err))
         
     @staticmethod    
     def clamp(n: float, minn: float, maxn: float) -> None:
